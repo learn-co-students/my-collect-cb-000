49409762550fde92af78ac6068f45630571ab40d
maxgrok <maxgrok2011@gmail.com>
2016-12-20 15:46:27 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
-
-
+def my_collect(array)
+	i = 0
+	collection = []
+	while i < array.length
+		yield (array[i])
+		if array[i] == "ruby" || array[i] == "javascript" || array[i] == "python" || array[i] == "objective-c"
+	 		word = array[i].to_s
+			word = word.upcase
+			collection.push(word)
+			i += 1
+		else
+			word = array[i].split(" ")
+			word = word.first
+			collection.push(word)
+			i += 1
+		end
+	end
+	collection
+end
