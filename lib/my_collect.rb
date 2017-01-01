52712c4b625410a7a342cb845d42756cdd20b93d
Drew Vollrath <andre@vollrath.ca>
2017-01-01 08:21:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array) # put argument(s) here
+  # code here
+  count, collection = 0, []
+  while count < array.length
+    collection << yield(array[count])
+    count += 1
+  end
+  collection
+end
