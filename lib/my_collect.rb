781614d4c66067d25ffdfb5926021aa6bf767f26
Kobie Jones <kobiedomo@gmail.com>
2017-02-25 21:48:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(names)
+  collection = []
+  i = 0
+  while i < names.length
+    collection << yield(names[i])
+    i+=1
+  end
+  collection
+end
 
-
+my_collect("") {|name| name.split(" ")}
