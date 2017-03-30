ea419ee86b43798527c8ef5a55838b84681118cc
Quinton Boltin <quinton@valleyexpetec.com>
2017-03-30 22:05:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  i = 0
+  rarr = []
+  while i < array.size
+    rarr << yield(array[i])
+    i += 1
+  end
+  rarr
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) {|i| i}
