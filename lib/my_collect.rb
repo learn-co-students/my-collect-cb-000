92cf1277638a742d98eff2e61c0dee401b39d3b4
Matthew McAnelly <mail@matthewmcanelly.com>
2017-04-11 13:34:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+# Yields members of a collection argument to a block and returns a new
+# collection based on the block.
+def my_collect(array)
+  collection = []
+  index = 0
+  while index < array.size
+    collection << yield(array[index])
+    index += 1
+  end
+  collection
+end
