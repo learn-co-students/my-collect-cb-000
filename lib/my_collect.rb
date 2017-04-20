fe7cefb0a4c646169fbdf2ee22860ffeb76dfa98
Alyssa Hogan <alyssabhogan@gmail.com>
2017-04-20 13:15:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+# Executes code called on each element of array and returns the new array
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
