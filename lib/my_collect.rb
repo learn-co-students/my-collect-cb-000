0748531c2ee77fbae5db73325cd78429dc4429d2
Devan Moylan <dmmoylan@gmail.com>
2017-05-04 21:13:32 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+def my_collect(array)
+  storage = []
+  i = 0
+  while i < array.length do
+    storage << yield(array[i])
+    i +=1
+  end
+  storage
+end
 
+array = []
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
