a3b69d9068919af37c1378f3a8ed379516f033e7
Patrick Townley <patricktownley@gmail.com>
2017-06-05 03:25:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(original_collection)
+  i = 0
+  new_collection = []
+  while i < original_collection.size
+    new_collection << yield(original_collection[i])
+    i += 1
+  end
+  new_collection
+end
