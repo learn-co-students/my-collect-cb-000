80a07362e54b47950771e3aeba093e837a306f3d
Mohammad Khan <sparrowangels@gmail.com>
2017-09-04 05:06:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+# take argument of a collection,
+# iterate over that collection using a while loop, and
+# execute the code in the block you call it with for each element in the collection (use the yield keyword).
+# It should return the modified collection.
 
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    collection = []
+    while i < array.length
+      collection << yield(array[i])
+      i+=1
+    end
+  else
+    puts "Hey! No block given!"
+  end #block_given?
+  collection
+end
