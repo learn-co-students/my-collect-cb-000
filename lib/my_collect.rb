bb319714de33ead72cc2710a607aca8de060b828
chandana M <chandu11288@gmail.com>
2018-02-09 19:22:19 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(arr)
+ counter = 0
+ new_list = []
+  while counter <arr.length
+    new_list << yield(arr[counter])
+    counter += 1
+  end
+  new_list
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
