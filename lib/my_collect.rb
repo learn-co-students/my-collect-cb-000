b6cdb1284c5b7474013750820d906243736540be
Allan Glasier <allan.glasier@outlook.com>
2018-03-29 02:51:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+collection = []
 
+def my_collect(collection)
+  i = 0
+  collected = []
+  while i < collection.length
+    collected << yield(collection[i])
+    i += 1
+  end
+  collected
+end
 
+my_collect(collection) do |x|
+  x
+end
