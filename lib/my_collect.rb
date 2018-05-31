9595bb7aa522ace9a4158a5d30cfaec7d93c82bf
Adam Urbina <adamurbina@gmail.com>
2018-05-31 19:18:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+    newArray = Array.new
+    index = 0
+    while index < array.size
+        newArray << yield(array[index])
+        index += 1
+    end
+    newArray
+end
