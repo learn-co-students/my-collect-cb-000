732e73799f72abf79ce79a5f716e29ca6354ed8a
Chris Franklin <cf73021@gmail.com>
2018-07-20 14:59:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+array = ["Jim", "Tim", "Jack"]
+def my_collect(array)
+  i = 0
+  collection = []
+  while array.length > i
+    collection << yield(array[i])
+    i +=1
+  end
+  collection
+end
 
-
+my_collect(array) do |item|
+  item.upcase
+end
