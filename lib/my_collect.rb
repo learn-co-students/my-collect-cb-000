3749f51fadafdfed47d0885c01c59d9f1d4d8d27
josh.e.stroud@gmail.com Stroud <josh.e.stroud@gmail.com>
2018-08-06 17:00:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if(array == [])
+    return array
+  else 
+    i = 0 
+    new_array = []
+    while i < array.size 
+      new_array << yield(array[i])
+      i += 1
+    end
+  end
+  new_array
+end
